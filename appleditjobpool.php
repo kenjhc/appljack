c32accdcc619817f673ae0bad23c1f7af522b8a0
Faraz Hussain <faraz.hussain@reavertechnologies.com>
2024-10-04 23:26:06 +0500

    READ MORE ADDED

--- appleditjobpool.php
+++ appleditjobpool.php
@@ -325,7 +325,7 @@ $customFields = $stmt->fetchAll(PDO::FETCH_ASSOC);
                                                                 <td><?= htmlspecialchars($nodeName) ?></td>
                                                                 <td>
                                                                     <div class="short-text">
-                                                                        <?php if (strlen($nodeValue) > 100): ?>
+                                                                        <?php if (strlen($nodeValue) > 5): ?>
                                                                             <?= htmlspecialchars(substr($nodeValue, 0, 100)) ?>
                                                                             <span class="dots">...</span>
                                                                             <span class="more-text"><?= htmlspecialchars($nodeValue) ?></span>
