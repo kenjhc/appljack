c43022ed12821e2ee8968418c01e34960f81f5b0
Faraz Hussain <faraz.hussain@reavertechnologies.com>
2024-10-23 20:45:48 +0500

    cron updated

--- applupdate2.sh
+++ applupdate2.sh
@@ -47,8 +47,8 @@ $NODE "$DIR/xmldl_all.js" >> "$LOGFILE" 2>&1
 echo "Running applfeedtransform.js" >> "$LOGFILE"
 $NODE "$DIR/applfeedtransform.js" >> "$LOGFILE" 2>&1
 
-echo "Running applupload8.js" >> "$LOGFILE"
-$NODE "$DIR/applupload8.js" >> "$LOGFILE" 2>&1
+echo "Running applupload9.js" >> "$LOGFILE"
+$NODE "$DIR/applupload9.js" >> "$LOGFILE" 2>&1
 
 echo "Running appljobs_delete.js" >> "$LOGFILE"
 $NODE "$DIR/appljobs_delete.js" >> "$LOGFILE" 2>&1
